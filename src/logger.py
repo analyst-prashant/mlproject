dac52d991c79ce9f73dd7806b66c95977426fd8d
analyst-prashant <analyst.prashantj@gmail.com>
2023-05-14 22:51:28 +0100

    EDA

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import logging
+from datetime_1 import datetime
 import os
-from datetime import datetime
+
 
 log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",log_file)
